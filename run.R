e948e1744fa533ee896870d247f9a2fa1d434578
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-02-10 12:31:53 +0800

    Fix timeout: Disable verbose console messages

--- run.R
+++ run.R
@@ -1,15 +1,19 @@
 library(etlTurtleNesting)
 library(wastdr)
 library(drake)
+library(ruODK)
 
+# drake::drake_cache(here::here(".drake"))$unlock()
 
 Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=FALSE)
 Sys.setenv(ODKC_IMPORT_UPDATE_MEDIA=TRUE)
+Sys.setenv(RU_VERBOSE=FALSE)
+Sys.setenv(WASTDR_VERBOSE=FALSE)
 drake::clean()
 drake::make(odkc2019(), lock_envir = FALSE)
 
 # Overwrite new records after importing new users into WAStD
-Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=TRUE)
+Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=FALSE)
 drake::clean()
 drake::make(odkc2020(), lock_envir = FALSE)
 
