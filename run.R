30e1cd94eeed297179fffa88980e3915155d4538
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-03-26 07:42:21 +0800

    Google sheets auth works using SAT as ADC

--- run.R
+++ run.R
@@ -4,27 +4,21 @@ library(drake)
 library(ruODK)
 library(googlesheets4)
 
-# After a failed run, unlock the cache
-# drake::drake_cache(here::here(".drake"))$unlock()
-
-# Overwrite new records after importing new users or adding aliases
-Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=TRUE)
-# Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=FALSE)
+# drake::drake_cache(here::here(".drake"))$unlock() # Recover failed run
+#
+Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=TRUE)        # After adding user aliases
+# Sys.setenv(ODKC_IMPORT_UPDATE_EXISTING=FALSE)     # Speedrun
 
 Sys.setenv(ODKC_IMPORT_UPLOAD_MEDIA=TRUE)
 Sys.setenv(RU_VERBOSE=FALSE)
 Sys.setenv(WASTDR_VERBOSE=FALSE)
 
-# Old Central instance odkcentral.dbca.wa.gov.au
-# will be sunset after 2020-21 season
-drake::clean()
-drake::make(odkc2019(), lock_envir = FALSE)
 
-# New ODK Central instance odkc.dbca.wa.gov.au
+drake::clean()
+drake::make(odkc2019(), lock_envir = FALSE)         # Sunset after 2020-21
 drake::make(odkc2020(), lock_envir = FALSE)
 
-# Generate reports
 # drake::clean("wastd_data")
-# drake::clean("wastd_reports")
 # drake::clean("wastd_tags")
+# drake::clean("wastd_reports")
 drake::make(etlTurtleNesting::wastd_reports())
