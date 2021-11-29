b9a1ec4e6f6a0e904dbad060daccdb80013c80fd
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-11-29 11:41:43 +0800

    Fix: ODKC TT nest tag excludes empty tags
    * filter by empty tag_label and comments
    * some folks place nest tag into comments

--- R/odkc_tt_as_wastd_nesttagobs.R
+++ R/odkc_tt_as_wastd_nesttagobs.R
@@ -28,7 +28,7 @@ odkc_tt_as_wastd_nesttagobs <- function(data) {
       comments = nest_nest_tag_comments
     ) %>%
     dplyr::filter_at(
-      dplyr::vars(tag_label),
+      dplyr::vars(tag_label, comments),
       dplyr::any_vars(!is.na(.))
     )
 }
