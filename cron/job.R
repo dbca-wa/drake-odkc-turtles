0958c7aff32fbccf6f9227a07e4d83809e2bfcc5
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-03-06 14:24:02 +0800

    Add config dir
    * Mount a persistent volume to /app/config
    * Place an .Renviron inside the persistent volume

--- cron/job.R
+++ cron/job.R
@@ -1,12 +1,21 @@
+# Setup -----------------------------------------------------------------------#
 library(sf)
 library(wastdr)
+library(magrittr)
+library(glue)
 
-fn_wastd_sites <- here::here("inst/wastd_sites.rds")
-fn_wastd_data <- here::here("inst/wastd_data.rds")
-fn_w2_data <- here::here("inst/w2_data.rds")
+fn_wastd_sites <- "/app/inst/wastd_sites.rds"
+fn_wastd_data <- "/app/inst/wastd_data.rds"
+fn_w2_data <- "/app/inst/w2_data.rds"
 
+# Gatechecks ------------------------------------------------------------------#
 print("Shared directory exists:")
-print(fs::dir_exists(here::here()))
+print(fs::dir_exists("/app/inst/"))
+
+readRenviron("/app/config/.Renviron")
+
+print("wastdr settings:")
+print(wastdr::wastdr_settings())
 
 # WAStD Areas and Sites - 10 sec ----------------------------------------------#
 
@@ -19,31 +28,31 @@ saveRDS(sites, file = fn_wastd_sites)
   glue::glue() %>%
   print()
 
-# # WAMTRAM data - 2 mins -------------------------------------------------------#
-# if (wastdr::w2_online() == FALSE) {
-#   "[{Sys.time()}] WAMTRAM not accessible. Need to run in DBCA intranet with credentials in env vars." %>%
-#     glue::glue() %>%
-#     print()
-# } else {
-#   "[{Sys.time()}] Downloading WAMTRAM Data to {fn_w2_data}" %>%
-#     glue::glue() %>%
-#     print()
-#
-#   w2_data <- wastdr::download_w2_data(save = fn_w2_data)
-#
-#   "[{Sys.time()}] WAMTRAM Data saved locally to folder {fn_w2_data}." %>%
-#     glue::glue() %>%
-#     print()
-# }
-#
-# # WAStD data - 40 mins --------------------------------------------------------#
-# "[{Sys.time()}] Downloading WAStD Data to {fn_wastd_data}" %>%
-#   glue::glue() %>%
-#   print()
-#
-# wastd_data <- wastdr::download_wastd_turtledata()
-# saveRDS(wastd_data, file = fn_wastd_data)
-#
-# "[{Sys.time()}] WAStD Data saved locally to folder {fn_wastd_data}." %>%
-#   glue::glue() %>%
-#   print()
+# WAMTRAM data - 2 mins -------------------------------------------------------#
+if (wastdr::w2_online() == FALSE) {
+  "[{Sys.time()}] WAMTRAM not accessible. Need to run in DBCA intranet with credentials in env vars." %>%
+    glue::glue() %>%
+    print()
+} else {
+  "[{Sys.time()}] Downloading WAMTRAM Data to {fn_w2_data}" %>%
+    glue::glue() %>%
+    print()
+
+  w2_data <- wastdr::download_w2_data(save = fn_w2_data)
+
+  "[{Sys.time()}] WAMTRAM Data saved locally to folder {fn_w2_data}." %>%
+    glue::glue() %>%
+    print()
+}
+
+# WAStD data - 40 mins --------------------------------------------------------#
+"[{Sys.time()}] Downloading WAStD Data to {fn_wastd_data}" %>%
+  glue::glue() %>%
+  print()
+
+wastd_data <- wastdr::download_wastd_turtledata()
+saveRDS(wastd_data, file = fn_wastd_data)
+
+"[{Sys.time()}] WAStD Data saved locally to folder {fn_wastd_data}." %>%
+  glue::glue() %>%
+  print()
